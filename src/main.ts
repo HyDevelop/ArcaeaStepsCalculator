3437f89219e072b6e36a89750f2f0ec477cb104d
Hykilpikonna <admin@moecraft.cc>
2020-06-09 12:11:54 -0400

    [+] Import string resource in main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,13 @@ Vue.use(ElementUI);
 import VueCookies from 'vue-cookies'
 Vue.use(VueCookies)
 
-new Vue({
+import VueI18n from "vue-i18n";
+Vue.use(VueI18n)
+
+// @ts-ignore
+import {t} from './res/strings.js'
+export const i18n = new VueI18n({locale: 'en', messages: t})
+
+new Vue({ i18n,
   render: h => h(App),
 }).$mount('#app')
