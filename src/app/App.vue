99604bd15745ed18a9323970232427e472fe2958
Hykilpikonna <admin@moecraft.cc>
2020-06-08 23:10:11 -0400

    [S] Set max width

--- src/app/App.vue
+++ src/app/App.vue
@@ -23,6 +23,8 @@
 <style lang="scss">
     #app
     {
+        max-width: 1000px;
+        margin: auto;
         text-align: center;
         font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
     }
