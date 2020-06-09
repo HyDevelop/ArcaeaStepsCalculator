fcfa5a50cf09d6ed4cdbc9605074ee14f3064816
Hykilpikonna <admin@moecraft.cc>
2020-06-09 12:11:40 -0400

    [+] Create string resorce js file

---
+++ src/res/strings.js
@@ -0,0 +1,48 @@
+export const t = {
+    en: {},
+    zh: {}
+}
+
+t.en.title = 'Arcaea Steps Calculator'
+
+// Dividers
+
+t.en.divCalculation = 'Calculation'
+
+t.en.divSettings = 'Settings'
+
+// Calculation labels
+
+t.en.lblTarget = 'How many steps do you want to go? (Decimal allowed)'
+
+t.en.lblResults = 'Results:'
+
+// Calculation Table labels
+
+t.en.tableTitle = 'Title'
+
+t.en.tableTime = 'Time'
+
+t.en.tableDiff = 'Diff'
+
+t.en.tableChar = 'Character'
+
+t.en.tableSteps = 'Steps'
+
+t.en.tableShift = 'Shift'
+
+// Settings labels
+
+t.en.lblDisplayLimit = 'Display Limit:'
+
+t.en.lblShiftRange = 'Shift Range:'
+
+t.en.lblCharSteps = 'Characters\' Step Values:'
+
+t.en.lblBoughtPacks = 'Bought Song Packs:'
+
+t.en.lblBoughtSongs = 'Bought Individual Songs:'
+
+t.en.lblLevelConfidence = 'Level Confidence:'
+
+t.en.lblChartConfidence = 'Individual Chart Confidence:'
