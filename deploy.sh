19cd6a7e13d93d9faf7ee2a742df348b16cedd24
Hykilpikonna <admin@moecraft.cc>
2020-06-09 12:41:16 -0400

    [+] Create deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'arcaea.hydev.org' > CNAME
+
+git init
+git add -A
+git commit -m 'update'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:HyDevelop/ArcaeaStepsCalculator.git master:gh-pages
+
+cd -
