a392c43aa0e9368cd31f332a0753e9e782712dc0
Hykilpikonna <admin@moecraft.cc>
2020-06-09 12:18:43 -0400

    [+] Create chinese translation

--- src/res/strings.js
+++ src/res/strings.js
@@ -4,45 +4,63 @@ export const t = {
 }
 
 t.en.title = 'Arcaea Steps Calculator'
+t.zh.title = 'Arcaea 爬梯子计算器'
 
 // Dividers
 
 t.en.divCalculation = 'Calculation'
+t.zh.divCalculation = '计算'
 
 t.en.divSettings = 'Settings'
+t.zh.divSettings = '设置'
 
 // Calculation labels
 
 t.en.lblTarget = 'How many steps do you want to go? (Decimal allowed)'
+t.zh.lblTarget = '要爬多少步w (可以用小数啦'
 
 t.en.lblResults = 'Results:'
+t.zh.lblResults = '找到的图:'
 
 // Calculation Table labels
 
 t.en.tableTitle = 'Title'
+t.zh.tableTitle = '曲名'
 
 t.en.tableTime = 'Time'
+t.zh.tableTime = '时长'
 
 t.en.tableDiff = 'Diff'
+t.zh.tableDiff = '难度'
 
 t.en.tableChar = 'Character'
+t.zh.tableChar = '搭档'
 
 t.en.tableSteps = 'Steps'
+t.zh.tableSteps = '步数'
 
 t.en.tableShift = 'Shift'
+t.zh.tableShift = '偏移'
 
 // Settings labels
 
 t.en.lblDisplayLimit = 'Display Limit:'
+t.zh.lblDisplayLimit = '显示多少个呢?'
 
 t.en.lblShiftRange = 'Shift Range:'
+t.zh.lblShiftRange = '步数偏移的最大值:'
 
 t.en.lblCharSteps = 'Characters\' Step Values:'
+t.zh.lblCharSteps = '你想用的搭档的 STEP 数值:'
 
 t.en.lblBoughtPacks = 'Bought Song Packs:'
+t.zh.lblBoughtPacks = '想用的曲包:'
 
 t.en.lblBoughtSongs = 'Bought Individual Songs:'
+t.zh.lblBoughtSongs = '想用的单曲:'
 
 t.en.lblLevelConfidence = 'Level Confidence:'
+t.zh.lblLevelConfidence = '难度大概分数:'
 
 t.en.lblChartConfidence = 'Individual Chart Confidence:'
+t.zh.lblChartConfidence = '谱面大概分数:'
