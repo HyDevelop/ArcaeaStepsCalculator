8198b70421087bf724e9a330e655fa3c0cdabff5
Hykilpikonna <admin@moecraft.cc>
2020-06-08 22:57:40 -0400

    [M] Move settings class to separate file

--- src/app/App.vue
+++ src/app/App.vue
@@ -6,48 +6,7 @@
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
-
-    class Settings
-    {
-        characterSteps = '102,90,76,55'
-        boughtPacks = {
-            'Arcaea': true,
-            'World Extend': false,
-            'Black Fate': false,
-            'Adverse Prelude': true,
-            'Luminous Sky': true,
-            'Vicious Labyrinth': true,
-            'Eternal Core': true,
-            'Sunset Radiance': false,
-            'Absolute Reason': false,
-            'Binary Enfold': false,
-            'Ambivalent Vision': false,
-            'Crimson Solace': false,
-            'CHUNITHM': false,
-            'Groove Coaster': false,
-            'Tone Sphere': false,
-            'Lanota': false,
-            'Dynamix': false,
-            'Memory Archive': false
-        }
-        boughtSongs = 'Astral tale\nAvant Raze'
-        levelConfidence = {
-            '1': 1000,
-            '2': 1000,
-            '3': 1000,
-            '4': 1000,
-            '5': 999,
-            '6': 998,
-            '7': 996,
-            '8': 994,
-            '9': 986,
-            '9+': 930,
-            '10': 940,
-            '10+': 0, // I don't want to use 10+ charts to climb steps
-            '11': 0
-        }
-        songConfidence = 'Monochrome Princess: 0\nLunarOrbit: 968\nKanagawa: 999'
-    }
+    import Settings from "@/app/settings";
 
     @Component
     export default class App extends Vue
